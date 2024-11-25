b9cb3827b94621f364135a9c3a6569e311747cbe
Gesporidgers <49988107+Gesporidgers@users.noreply.github.com>
2024-11-25 12:37:44 +0300

    changed carousel component

--- lr_web2.client/src/assets/carousel.jsx
+++ lr_web2.client/src/assets/carousel.jsx
@@ -1,3 +1,14 @@
-export default const Carousel = (props) =>{
-
+export default function Carousel(props) {
+    const {children} = props;
+    return (
+        <div className="carousel-container">
+            <div className="carousel-wrapper">
+                <div className="carousel-content-wrapper">
+                    <div className="carousel-content">
+                    {children}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
