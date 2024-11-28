76a3f34ee20e8fb5d197ee92601549347a99cd31
Gesporidgers <49988107+Gesporidgers@users.noreply.github.com>
2024-11-28 08:59:30 +0300

    Finalized section "Contact"

--- lr_web2.client/src/Pages/contact.jsx
+++ lr_web2.client/src/Pages/contact.jsx
@@ -1,25 +1,32 @@
-import { Box, Button, TextArea, TextField } from "@radix-ui/themes";
+import { Box, Button, TextArea, TextField, TabNav, Table, Strong, Text } from "@radix-ui/themes";
 
 export default function Contact() {
     return (
         <>
-            <h1>Контакты</h1>
-            <table border="1pt">
-                <tr>
-                    <td><b>Единый центр бронирования</b></td>
-                    <td>8(495)223-92-76 (с 9 до 19 ч. пн-пт)</td>
-                </tr>
-                <tr>
-                    <td><b>Мы в социальных сетях</b></td>
-                    <td>Мы в Инстаграм: @rutravelexpert<br />Мы Вконтакте: @rutravelexpert </td>
-                </tr>
-                <tr>
-                    <td><b>Email</b></td>
-                    <td><a href="mailto:zakaz.goldenmiddle@mail.ru  ">zakaz.goldenmiddle@mail.ru </a></td>
-                </tr>
-                <tr>
-                    <td><b>Реквизиты</b></td>
-                    <td><b>ООО "Золотая середина"</b><br />
+            <TabNav.Root>
+                <TabNav.Link href='/'>Главная</TabNav.Link>
+                <TabNav.Link href='catalog'>Каталог</TabNav.Link>
+                <TabNav.Link href='buy'>Как купить</TabNav.Link>
+                <TabNav.Link href='about'>О нас</TabNav.Link>
+                <TabNav.Link active>Контакты</TabNav.Link>
+            </TabNav.Root>
+            <h1 style={{ paddingLeft: '10px' }}>Контакты</h1>
+            <Table.Root variant='surface' style={{ maxWidth: '500px', marginLeft: '10px' }}>
+                <Table.Row>
+                    <Table.RowHeaderCell><Text as="p"><Strong>Единый центр бронирования</Strong></Text></Table.RowHeaderCell>
+                    <Table.Cell>8(495)223-92-76 (с 9 до 19 ч. пн-пт)</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.RowHeaderCell><Text as="p"><Strong>Мы в социальных сетях</Strong></Text></Table.RowHeaderCell>
+                    <Table.Cell>Мы в Инстаграм: @rutravelexpert<br />Мы Вконтакте: @rutravelexpert </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.RowHeaderCell><Text as="p"><Strong>Email</Strong></Text></Table.RowHeaderCell>
+                    <Table.Cell><a href="mailto:zakaz.goldenmiddle@mail.ru  ">zakaz.goldenmiddle@mail.ru </a></Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.RowHeaderCell><Text as="p"><Strong>Реквизиты</Strong></Text></Table.RowHeaderCell>
+                    <Table.Cell><b>ООО "Золотая середина"</b><br />
                         Генеральный директор: Мохнаткина Екатерина Витальевна<br />
                         Адрес: БЦ Лотос, 117638, г. Москва, ул. Одесская д. 2, пом. 1/2
                         <br />
@@ -29,24 +36,31 @@ export default function Contact() {
                         ФИЛИАЛ "Центральнный" БАНКА ВТБ (ПАО) г. Москва. <br />
                         К/с: 30101810145250000411
                         БИК 044525411 <br />
-                        ОГРН 1117746112461
-                    </td>
-                </tr>
-            </table>
-            <hr />
-            <h2>Напишите нам</h2>
-            <Box>
-                <Box maxWidth="250px">
-                    <TextField.Root type="text" id="form" placeholder="Имя" /></Box>
-                    <Box maxWidth="250px">
-                <TextField.Root type="email" id="form" placeholder="Email" /></Box>
-                <Box maxWidth="250px"> <TextField.Root type="text" id="form" placeholder="Тема обращения" /></Box>
-                <Box maxWidth="250px"><TextArea resize='vertical' placeholder='Напишите тут своё сообщение'/></Box>
-                
-                <Box maxWidth="250px"><Button type="submit" onClick={(event) => event.preventDefault()}>Отправить</Button></Box>
-                
-            </Box>
+                        ОГРН 1117746112461</Table.Cell>
+                </Table.Row>
+            </Table.Root>
 
+            <hr />
+            <h2 style={{ marginLeft: '10px' }}>Напишите нам</h2>
+            <Box px='10px'>
+                <Box maxWidth="250px" mb='2'>
+                    <TextField.Root type="text" id="form" placeholder="Имя" /></Box>
+                <Box maxWidth="250px" mb='2'>
+                    <TextField.Root type="email" id="form" placeholder="Email" />
+                </Box>
+                <Box maxWidth="250px" mb='2'>
+                    <TextField.Root type="text" id="form" placeholder="Тема обращения" />
+                </Box>
+                <Box maxWidth="250px" mb='2'>
+                    <TextArea resize='vertical' placeholder='Напишите тут своё сообщение' />
+                </Box>
+
+                <Box maxWidth="250px" mb='2'>
+                    <Button type="submit" onClick={(event) => event.preventDefault()}>Отправить</Button>
+                </Box>
+
+            </Box>
+            <br />
         </>
     );
 };
